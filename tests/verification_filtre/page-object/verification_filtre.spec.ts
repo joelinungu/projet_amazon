d80aa480a2fd8b29fd3f7b7526bfe4df5dec907c
PhilippeKevin <113717062+PhilippeKevin@users.noreply.github.com>
2024-11-04 13:37:19 +0100

    update

--- tests/verification_filtre/page-object/verification_filtre.spec.ts
+++ tests/verification_filtre/page-object/verification_filtre.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '../page-object/fixture'; 
+import { test } from '../page-object/fixture'; // Modifier le chemin vers votre fichier étendu
 
 test('verification filtre', async ({ verif_filtre }) => {
     await verif_filtre.page.goto('https://www.amazon.fr/');
@@ -8,9 +8,12 @@ test('verification filtre', async ({ verif_filtre }) => {
 
     // Rechercher un produit
     await verif_filtre.searchInput.click(); // Utilisez la méthode définie dans Verif
-    await verif_filtre.searchItem('ordinateur portable'); // Utilisez la méthode définie dans Verif
-    await verif_filtre.page.getByPlaceholder('Rechercher Amazon.fr').press('Enter'); // Utilisez page de verif_filtre
-    await verif_filtre.page.getByRole('link', { name: 'AMD Ryzen 9' }).click(); // Utilisez page de verif_filtre
-    await verif_filtre.page.getByRole('link', { name: '″ (43 cm) et plus' }).click(); // Utilisez page de verif_filtre
-    await verif_filtre.page.getByRole('link', { name: '2 To' }).click(); // Utilisez page de verif_filtre
+    await verif_filtre.page.getByPlaceholder('Rechercher Amazon.fr').click();
+    await verif_filtre.page.getByPlaceholder('Rechercher Amazon.fr').fill('ordinateur portable');
+    await verif_filtre.page.getByPlaceholder('Rechercher Amazon.fr').press('Enter');
+    await verif_filtre.page.getByRole('button', { name: 'Go', exact: true }).click();
+    await verif_filtre.page.getByRole('link', { name: 'AMD Ryzen 9' }).click();
+    await verif_filtre.page.getByRole('link', { name: 'à 16″ (38 à 42 cm)' }).click();
+    await verif_filtre.page.getByRole('link', { name: '32 Go', exact: true }).click();
+    await verif_filtre.page.getByLabel('Technologie de connectivité').getByRole('link', { name: 'HDMI' }).click();
 });
